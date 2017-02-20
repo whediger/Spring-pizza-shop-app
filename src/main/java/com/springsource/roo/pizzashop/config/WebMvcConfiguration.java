4386ef7710d6dc3a90c0c50bcb1dc95749f6b84a
Wes Hediger <wes.hediger@gmail.com>
2017-02-20 13:26:22 -0700

    add web tier

---
+++ src/main/java/com/springsource/roo/pizzashop/config/WebMvcConfiguration.java
@@ -0,0 +1,12 @@
+package com.springsource.roo.pizzashop.config;
+import org.springframework.roo.addon.web.mvc.controller.annotations.config.RooWebMvcConfiguration;
+
+/**
+ * = WebMvcConfiguration
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooWebMvcConfiguration
+public class WebMvcConfiguration {
+}
---
+++ src/main/java/com/springsource/roo/pizzashop/config/jackson/DomainModelModule.java
@@ -0,0 +1,12 @@
+package com.springsource.roo.pizzashop.config.jackson;
+import org.springframework.roo.addon.web.mvc.controller.annotations.config.RooDomainModelModule;
+
+/**
+ * = DomainModelModule
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooDomainModelModule
+public class DomainModelModule {
+}
