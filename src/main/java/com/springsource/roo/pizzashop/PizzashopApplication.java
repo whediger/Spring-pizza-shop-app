ed936997d299fccdb8bf993ad2fb3da47bc0924d
Wes Hediger <wes.hediger@gmail.com>
2017-02-20 12:22:47 -0700

    create top level package

---
+++ src/main/java/com/springsource/roo/pizzashop/PizzashopApplication.java
@@ -0,0 +1,23 @@
+package com.springsource.roo.pizzashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = PizzashopApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class PizzashopApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(PizzashopApplication.class, args);
+    }
+}
