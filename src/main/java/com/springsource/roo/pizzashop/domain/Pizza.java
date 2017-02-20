b7b60746b6bea47a4a73ae6c872e076b168cc1dd
Wes Hediger <wes.hediger@gmail.com>
2017-02-20 14:54:46 -0700

    add fields and entities

---
+++ src/main/java/com/springsource/roo/pizzashop/domain/Pizza.java
@@ -0,0 +1,22 @@
+package com.springsource.roo.pizzashop.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Pizza {
+
+    /**
+     */
+    @NotNull
+    @Size(min = 2)
+    private String name;
+
+    /**
+     */
+    private Float price;
+}
