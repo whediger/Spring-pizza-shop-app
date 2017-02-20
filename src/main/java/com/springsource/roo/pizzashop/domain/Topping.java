a1472c3ab4e31ec47dd3ae57fdd70399502c08cd
Wes Hediger <wes.hediger@gmail.com>
2017-02-20 12:35:47 -0700

    add entity Topping

---
+++ src/main/java/com/springsource/roo/pizzashop/domain/Topping.java
@@ -0,0 +1,16 @@
+package com.springsource.roo.pizzashop.domain;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+import org.springframework.roo.addon.javabean.annotations.RooToString;
+import org.springframework.roo.addon.jpa.annotations.entity.RooJpaEntity;
+
+/**
+ * = Topping
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooJavaBean
+@RooToString
+@RooJpaEntity
+public class Topping {
+}
